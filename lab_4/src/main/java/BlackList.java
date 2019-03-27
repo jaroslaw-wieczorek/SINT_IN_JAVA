28e27742096fd4159180946e850300f7f8c29810
Jarosław Wieczorek <jaroslaw.wieczorek@sealcode.org>
2019-03-27 22:29:00 +0100

    Update lab3 and add lab4

--- lab_4/src/main/java/BlackList.java
+++ lab_4/src/main/java/BlackList.java
@@ -1,2 +1,33 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.LinkedList;
+
+
 public class BlackList {
+
+    LinkedList<String> blackList = new LinkedList<String>();
+
+
+    public LinkedList<String> getBlackList(){
+        String blackListFile = "blacklist.txt";
+        String line;
+        BufferedReader br;
+
+        try {
+            br = new BufferedReader(new FileReader(blackListFile));
+
+            while ((line = br.readLine()) != null) {
+                if (!blackList.contains(line)){
+                    blackList.add(line);
+                }
+            }
+            br.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return  blackList;
+    }
+
 }
